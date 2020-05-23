66050b9b5b03994bfb42cdd86db2e8d827febb50
Kshitij Aggarwal <33875155+kshitijaggrwl@users.noreply.github.com>
2020-05-23 21:37:58 +0530

    ToDo List
    To do List using hooks and spread operator.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,32 @@
 import React, { useState } from "react";
 
-
-
-
-
 function App() {
-  
-  function handleClick(){
-  
+  const [arr, pusharr] = useState([]);
+  const [item, setItem] = useState("");
+  function handleClick() {
+    pusharr(prev => {
+      return [...prev, item];
+    });
+    setItem("");
   }
-  function handleList(event){
-  
+  function handleList(event) {
+    const value = event.target.value;
+    setItem(value);
   }
-
+  const listItems = arr.map(item => <li>{item}</li>);
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input onChange={handleList} type="text" />
+        <input onChange={handleList} type="text" value={item} />
         <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
-        <ul>
-          <li>A Item </li>
-        </ul>
+        <ul>{listItems}</ul>
       </div>
     </div>
   );
